3ba1dcb00a622d3b5b5e5c92d7fe49a1a47c6beb
dhruv <dhruv.panwar@brightlifecare.com>
2018-06-29 18:05:06 +0530

    changes

--- src/main/java/com/hk/logistics/config/SecurityConfiguration.java
+++ src/main/java/com/hk/logistics/config/SecurityConfiguration.java
@@ -59,7 +59,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-            .csrf()
+            .csrf().ignoringAntMatchers("/pincode/**","/storeOrder/**")
             .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
         .and()
             .addFilterBefore(corsFilter, CsrfFilter.class)
